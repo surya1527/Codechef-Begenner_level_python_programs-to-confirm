bc87f0a24f1ef722e6507b14fe781ad7a63c7472
Suryavamshi Sirigadhi <58841159+surya1527@users.noreply.github.com>
2021-06-24 21:01:47 +0530

    Add files via upload

---
+++ First and Last Digit.py
@@ -0,0 +1,6 @@
+
+t=int(input())          #enter the number of testcases
+while t>0:              
+    l= input()
+    print(int(l[0]) + int(l[-1]))           #0 is the index of first and -1 is the integer of last
+    t=t-1
