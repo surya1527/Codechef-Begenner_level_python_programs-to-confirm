667d637ceb6a6fc31d8b13a6bc486c5f79860013
Suryavamshi Sirigadhi <58841159+surya1527@users.noreply.github.com>
2021-06-24 21:06:41 +0530

    Add files via upload

---
+++ Turbo Sort.py
@@ -0,0 +1,10 @@
+t=int(input())      #enter the number of testcases
+l=[]                    #initialize the list
+for i in range(t):
+    n=int(input())      #input the number
+    l.append(n)         #append the interger in the list
+l.sort()                # sort the elements in the list
+for j in l:         
+        print(j)
+    
+    
