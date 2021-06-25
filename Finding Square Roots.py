7ac1b7b55a265565c932a6c5a404235b90434df6
Suryavamshi Sirigadhi <58841159+surya1527@users.noreply.github.com>
2021-06-25 22:10:37 +0530

    Add files via upload

---
+++ Finding Square Roots.py
@@ -0,0 +1,6 @@
+import math     #import the math module
+t = int(input())
+for i in range (t):
+    n = int(input())
+    sq = math.sqrt(n)           #finds the squareroot value
+    print(round(sq))
