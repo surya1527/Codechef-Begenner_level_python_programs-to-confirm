ddce74eb5563a652f0e71890735c6836ad15ed09
Suryavamshi Sirigadhi <58841159+surya1527@users.noreply.github.com>
2021-06-26 20:22:01 +0530

    Add files via upload

---
+++ Chef And Operators.py
@@ -0,0 +1,9 @@
+t=int(input())                                  #enter the number of testcases
+for i in range (t):                             
+    A , B = map(int,input().split())            #two input values in same
+    if (A < B):
+        print ("<")                             
+    elif (A > B):
+        print(">")
+    elif (A == B):
+        print("=")
