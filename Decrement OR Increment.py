b4719f31336dc9382867b4c82c567829db5b2cd2
Suryavamshi Sirigadhi <58841159+surya1527@users.noreply.github.com>
2021-06-27 22:01:24 +0530

    Add files via upload

---
+++ Decrement OR Increment.py
@@ -0,0 +1,8 @@
+N= int(input())             #enter the number of testcases
+if (N%4 == 0):              
+    N+=1                    #INCREMENTING
+else:
+    N-=1                    #DECREMENTING
+
+
+print(N)
