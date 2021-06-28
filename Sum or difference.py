e431225434045a1b374e3f6446f06f4fb4f846c2
Suryavamshi Sirigadhi <58841159+surya1527@users.noreply.github.com>
2021-06-28 21:45:20 +0530

    Add files via upload

---
+++ Sum or difference.py
@@ -0,0 +1,6 @@
+n1= int(input())   #enter the 1st input
+n2= int(input())   #enter the 2nd input
+if (n1>n2):        #relational operator
+    print(n1-n2)   #difference of inputs if n1>n2
+else:              
+    print(n1+n2)   #sum of inputs if n1<n2
